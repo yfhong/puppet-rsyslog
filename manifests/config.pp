84ae42f90d3b875f1b500373b2d1dc0b68ef36b1
Hong Yifan <hyfing@gmail.com>
2015-06-04 09:34:47 +0200

    use tempalte

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,6 +4,10 @@
 #
 class rsyslog::config {
 
+  $udp_enabled = $::rsyslog::use_udp
+  $tcp_enabled = $::rsyslog::use_tcp
+  $listen_address = $::rsyslog::listen_address
+
   file { $::rsyslog::logdir:
     ensure => 'directory',
     owner  => 'root',
@@ -12,51 +16,10 @@ class rsyslog::config {
   }
 
   file { $::rsyslog::conffile:
-    ensure => 'present',
-    owner  => 'root',
-    group  => $::rsyslog::params::root_group,
-    mode   => '0640',
-  }
-
-  if ($::rsyslog::use_udp) {
-    file_line { 'load_udp_mode':
-      path  => $::rsyslog::conffile,
-      line  => '\$ModLoad imudp',
-      match => ".*ModLoad\s+imudp.*",
-    }
-
-    file_line { 'set_udp_port':
-      path  => $::rsyslog::conffile,
-      line  => '\$UDPServerRun 514',
-      match => "[#\s]*\$UDPServerRun.*",
-    }
-
-    file_line { 'set_udp_bind_address':
-      path  => $::rsyslog::conffile,
-      line  => "\$UDPServerAddress ${::rsyslog::listen_address}",
-      match => ".*UDPServerAddress.*",
-      after => '\$UDPServerRun 514',
-    }
-  }
-
-  if ($::rsyslog::use_tcp) {
-    file_line { 'load_tcp_mode':
-      path  => $::rsyslog::conffile,
-      line  => '\$ModLoad imtcp',
-      match => ".*ModLoad\s+imtcp.*",
-    }
-
-    file_line { 'set_tcp_port':
-      path  => $::rsyslog::conffile,
-      line  => '\$TCPServerRun 514',
-      match => "[#\s]*\$TCPServerRun.*",
-    }
-
-    file_line { 'set_tcp_bind_address':
-      path  => $::rsyslog::conffile,
-      line  => "\$TCPServerAddress ${::rsyslog::listen_address}",
-      match => ".*TCPServerAddress.*",
-      after => '\$TCPServerRun 514',
-    }
+    ensure  => 'present',
+    owner   => 'root',
+    group   => $::rsyslog::params::root_group,
+    mode    => '0640',
+    content => template('rsyslog/rsyslog.conf.erb'),
   }
 }
